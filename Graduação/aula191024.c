2e2a4b8d692af26ad64042388190d9ebe95f1ad3
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2024-10-19 21:13:24 -0300

    Criando arquivo

---
+++ Graduação/aula191024.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+/*1) Leia 5 valores para uma variável A. A seguir mostre quantos valores digitados foram pares,
+quantos valores digitados foram ímpares, quantos foram positivos e quantos foram negativos.*/
+
+/*2) Calcule e mostre a soma dos números pares entre 1 e 100, inclusive.*/
