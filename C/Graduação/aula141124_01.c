1335a19c89b310a8e849ee1189594a3df52b2bd4
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2024-12-27 21:31:46 -0300

    Resolvendo exercício

--- C/Graduação/aula141124_01.c
+++ C/Graduação/aula141124_01.c
@@ -1,28 +1,30 @@
 // Defina um programa em C que crie uma matriz[3][3] de inteiros e preencha com valores digitados pelo 
-//usuário. Em seguida, o programa deve imprimir a matriz e a soma das matrizes.
+//usuário. Em seguida, o programa deve imprimir a matriz e a soma dos valores da matriz.
 
 #include <stdio.h>
 
 int main(){
-    int matriz[3][3];
-    int i, j;
-    int soma = 0;
+    //[Linha][Coluna]
+    int matriz1[3][3];
+    int i, j, soma = 0;
 
     for (i = 0; i < 3; i++){ //Percorre a matriz e atribui valores
         for (j = 0; j < 3; j++){
             printf("Digite o valor da matriz[%d][%d]: ", i, j);
-            scanf("%d", &matriz[i][j]);
+            scanf("%d", &matriz1[i][j]);
         }
     }
     
-    printf("\nMatriz:\n"); //Imprime a diagonal principal
+    printf("\nMatriz:\n"); //Imprime a matriz
     for (i = 0; i < 3; i++){
-        printf("%d ", matriz[i][i]);
-        soma = matriz[i][i] + soma;
+        for (j = 0; j < 3; j++){
+            printf("%d ", matriz1[i][j]);
+            soma += matriz1[i][j];            
+        }
         printf("\n");
     }
 
-    printf("\nSoma da diagonal principal: %d\n", soma);
+    printf("\nSoma: %d\n", soma);
 
     return 0;
 }
